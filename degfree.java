a763257a732d76a8c21c22525d84517588115eb7
juanitacortesc <124004073+juanitacortesc@users.noreply.github.com>
2023-04-17 19:22:44 -0400

    Add files via upload

---
+++ degfree.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author juanitacortes
+ */
+import java.util.Scanner;
+public class degfree {
+    
+    public void calc_littering(float males_trash,float females_trash,float litters,float deposits) 
+    {
+        float total = males_trash + females_trash;
+        float males_who_litter, females_who_litter;
+        
+        males_who_litter = (males_trash/total) * ((litters/total*100));
+        females_who_litter = (females_trash/total) * ((litters/total*100));
+        System.out.println("Total littering of males: " + males_who_litter);
+        System.out.println("Total littering of females" + females_who_litter);  
+    }
+    public static void main(String args[])
+    {
+            float T_male, T_female, T_deposits, T_litter;
+            
+            degfree obj = new degfree();
+            Scanner scan =  new Scanner(System.in);
+            
+            System.out.println("Enter the total of males: ");
+            T_male = scan.nextInt();
+            System.out.println("Enter the total of females: ");
+            T_female = scan.nextInt();
+            System.out.println("Enter the total of deposits: ");
+            T_deposits = scan.nextInt();
+            System.out.println("Enter the total of litters: ");
+            T_litter = scan.nextInt();
+            
+            obj.calc_littering(T_male, T_female, T_litter, T_deposits);
+    }
+}
